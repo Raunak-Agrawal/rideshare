251bf194aba0a22956870540d8c13ed2b4b689c5
Raunak Agrawal <raunk325@gmail.com>
2022-02-06 16:47:49 +0530

    initial commit2

--- config/apikeys.js
+++ config/apikeys.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+console.log(process.env.API_KEY, "APIKEY");
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
