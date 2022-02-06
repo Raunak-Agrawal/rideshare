17960a6d0417de417626871718afda090e7418eb
Raunak Agrawal <raunk325@gmail.com>
2022-02-06 16:51:09 +0530

    initial commit3

--- config/apikeys.js
+++ config/apikeys.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-console.log(process.env.API_KEY, "APIKEY");
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER,
-  appId: process.env.APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
